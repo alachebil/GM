fe03a9e43ff81adc3e5262cfad139ba64822bca6
alachebil <ala.chebil@esprit.tn>
2024-12-21 11:32:26 +0100

    estimation + ci/cd

--- src/test/java/com/example/intershipmanagement/IntershipManagementApplicationTests.java
+++ src/test/java/com/example/intershipmanagement/IntershipManagementApplicationTests.java
@@ -22,7 +22,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-
+// test 
 @ExtendWith(MockitoExtension.class)
 class IntershipManagementApplicationTests {
 
